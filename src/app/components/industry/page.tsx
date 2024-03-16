545ea5d838aa6ad6c69e6bcb5046239a0874c3a5
SatkarNiraula <satkarniraula69@gmail.com>
2024-03-16 17:10:27 +0545

    image blur effect in services

--- src/app/components/industry/page.tsx
+++ src/app/components/industry/page.tsx
@@ -11,42 +11,22 @@ const IndustryPage = () => {
                 Industries We Serve
               </h1>
             </a>
-            <p className="mb-16 text-lg">
+            <p className="text-lg">
               We envision a future where businesses seamlessly.
             </p>
           </div>
           <div className="flex flex-wrap h-full w-full">
-            <div className="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60 ">
-              <Image
-                src={"/calculator.png"}
-                height={1000}
-                width={1000}
-                alt=""
-              />
+            <div className="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60 h-auto max-w-lg transition-all duration-300 rounded-lg blur-none hover:blur-sm">
+              <Image src={"/software.png"} height={1000} width={1000} alt="" />
             </div>
-            <div className="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60 ">
-              <Image
-                src={"/calculator.png"}
-                height={1000}
-                width={1000}
-                alt=""
-              />
+            <div className="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60 h-auto max-w-lg transition-all duration-300 rounded-lg blur-none hover:blur-sm">
+              <Image src={"/marketing.png"} height={1000} width={1000} alt="" />
             </div>
-            <div className="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60 ">
-              <Image
-                src={"/calculator.png"}
-                height={1000}
-                width={1000}
-                alt=""
-              />
+            <div className="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60 h-auto max-w-lg transition-all duration-300 rounded-lg blur-none hover:blur-sm">
+              <Image src={"/video.png"} height={1000} width={1000} alt="" />
             </div>
-            <div className="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60 ">
-              <Image
-                src={"/calculator.png"}
-                height={1000}
-                width={1000}
-                alt=""
-              />
+            <div className="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60 h-auto max-w-lg transition-all duration-300 rounded-lg blur-none hover:blur-sm">
+              <Image src={"/software.png"} height={1000} width={1000} alt="" />
             </div>
           </div>
         </div>
--- src/app/components/statistics/page.tsx
+++ src/app/components/statistics/page.tsx
@@ -1,28 +1,28 @@
 const StatisticsPage = () => {
   return (
     <>
-      <section className="text-gray-600 body-font">
+      <section className="text-gray-600 body-font mt-[30px]">
         <div
-          className=" p-4 md:p-8 light:bg-gray-800 light:text-black-200 mx-auto mb-[50px]"
+          className=" p-4 md:p-8 light:bg-gray-800 light:text-black-200 mx-auto mb-[30px]"
           id="stats"
           role="tabpanel"
           aria-labelledby="stats-tab"
         >
           <dl className="flex justify-center gap-x-32 text-gray-700">
             <div className="flex flex-col items-center justify-center">
-              <dt className="text-[64px] font-extrabold">+20</dt>
+              <dt className="text-[44px] font-extrabold">+20</dt>
               <dd className="mt-[-16px] font-bold text-[24px]">Projects</dd>
             </div>
             <div className="flex flex-col items-center justify-center">
-              <dt className="text-[64px] font-extrabold">+50M</dt>
+              <dt className="text-[44px] font-extrabold">+50M</dt>
               <dd className="mt-[-16px] font-bold text-[24px]">Line Of Code</dd>
             </div>
             <div className="flex flex-col items-center justify-center">
-              <dt className="text-[64px] font-extrabold">+50</dt>
+              <dt className="text-[44px] font-extrabold">+50</dt>
               <dd className="mt-[-16px] font-bold text-[24px]">Team</dd>
             </div>
             <div className="flex flex-col items-center justify-center">
-              <dt className="text-[64px] font-extrabold">&#8734;</dt>
+              <dt className="text-[44px] font-extrabold">&#8734;</dt>
               <dd className="mt-[-16px] font-bold text-[24px]">
                 Opportunities
               </dd>
