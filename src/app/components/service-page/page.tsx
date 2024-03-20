0c846132616dcc3f2ae40a43f040aa53fe395246
Rohit Shrestha <106756449+RohitSth@users.noreply.github.com>
2024-03-20 20:02:13 +0545

    finalized all the responsiveness

--- src/app/components/service-page/page.tsx
+++ src/app/components/service-page/page.tsx
@@ -6,6 +6,19 @@ const ServicePage = () => {
       <section id="service-page" className="text-gray-600 body-font">
         <div className="container px-5 py-24 mx-auto">
           <div className="flex flex-wrap -m-4">
+            <div className="p-4 lg:w-1/4 md:w-1/2">
+              <div>
+                <h1 className="title-font text-4xl mb-4 font-bold text-black ">
+                  Our Services
+                </h1>
+                <p className="mb-16 text-sm">
+                  We envision a future where businesses seamlessly.
+                </p>
+                <div className="text-blue-500 text-sm hover:underline">
+                  <a href="#">See all services &gt;</a>
+                </div>
+              </div>
+            </div>
             <div className="lg:w-1/4 md:w-1/2 p-4">
               <div className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow-lg">
                 <a href="#">
