0636726ddd266c6431e0350a16e75aba54b13266
Rohit Shrestha <106756449+RohitSth@users.noreply.github.com>
2024-03-28 21:57:45 +0545

    completed the front page along with carousel

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -2,24 +2,10 @@ const Header = () => {
   return (
     <header className="text-black-200 body-font">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-        <a
-          href="/"
-          className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full"
-            viewBox="0 0 24 24"
-          >
-            <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-          </svg>
-          <span className="ml-3 text-lg">nemo</span>
+        <a href="/">
+          <img src="/logo1.png" alt="logo" className="w-16 h-16" />
         </a>
+
         <nav className="ml-auto flex flex-wrap items-center justify-center text-lg">
           <a href="/" className="mr-5 hover:text-[#0D7ABF]">
             Home
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,17 @@
-import Header from "./components/header/page";
-import Hero from "./components/hero/page";
-import StatisticsPage from "./components/statistics/page";
-import ServicePage from "./components/service-page/page";
-import Content from "./components/content/page";
-import IndustryPage from "./components/industry/page";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import StatisticsPage from "./components/Statistics";
+import ServicePage from "./components/ServicePage";
+import Content from "./components/Content";
+import IndustryPage from "./components/Industry";
 import Customer from "./components/customer/page";
-import Testimonial from "./components/testimonial/page";
-import Footer from "./components/footer/page";
-import Info from "./components/info/page";
+import Testimonial from "./components/Testimonial";
+import Footer from "./components/Footer";
+import Info from "./components/Info";
 
 export default function Home() {
   return (
-    <main className="mx-0 overflow-x-hidden">
+    <main className="overflow-x-hidden">
       <Info />
       <Header />
       <Hero />
